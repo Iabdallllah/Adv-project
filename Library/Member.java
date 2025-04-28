ff0b90d12bfa55de4ad561fd4c944d3e104d791e
Abdallah Ahmed <148303169+Iabdallllah@users.noreply.github.com>
2025-04-28 09:29:15 +0300

    Update Member.java

--- Library/Member.java
+++ Library/Member.java
@@ -12,23 +12,23 @@ public class Member extends User {
         if (!book.isAvailable()) {
             throw new Exception("Book is not available for borrowing");
         }
-        borrowedBooks.add(book);
-        book.setAvailable(false);
+        borrowedBooks.add(book); // Add book to borrowed list
+        book.setAvailable(false); // Mark book as borrowed
     }
 
     public void returnBook(Book book) throws Exception {
         if (!borrowedBooks.contains(book)) {
             throw new Exception("This book was not borrowed by this member");
         }
-        borrowedBooks.remove(book);
-        book.setAvailable(true);
+        borrowedBooks.remove(book); // Remove book from borrowed list
+        book.setAvailable(true); // Mark book as available
     }
 
     public ArrayList<Book> getBorrowedBooks() {
         return borrowedBooks;
     }
-
-    @Override
+    // Override the displayInfo method to show member details
+    @Override 
     public void displayInfo() {
         System.out.println("Member Name: " + getName() + ", ID: " + getUserID());
         System.out.println("Borrowed Books: " + borrowedBooks.size());
